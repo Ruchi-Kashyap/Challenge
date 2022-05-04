8f51a7a2378d47b533f0974ae163a6e65104bd99
Ruchi-Kashyap <104854045+Ruchi-Kashyap@users.noreply.github.com>
2022-05-04 16:42:47 +0530

    Add files via upload

---
+++ action_page.php
@@ -0,0 +1,14 @@
+
+<html>
+<body>
+
+Register no: <?php echo $_POST["Registerno"]; ?><br>
+Student Name  is: <?php echo $_POST["Sname"]; ?>
+English marks  is: <?php echo $_POST["Eng"]; ?>
+Language marks is: <?php echo $_POST["lng"]; ?>
+Physics marks   is: <?php echo $_POST["Phy"]; ?>
+chemistry marks  is: <?php echo $_POST["chem"]; ?>
+Maths marks is: <?php echo $_POST["math"]; ?>
+
+</body>
+</html>
